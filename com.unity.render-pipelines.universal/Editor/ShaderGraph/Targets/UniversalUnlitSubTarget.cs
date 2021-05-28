c7473e3872d82a9018cac292b7e95a68d5c2fb14
Matt Dean <matt@triband.net>
2021-05-28 13:18:10 +0200

    Per pass render state overrides

--- com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalUnlitSubTarget.cs
+++ com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalUnlitSubTarget.cs
@@ -8,29 +8,37 @@ using UnityEngine.Rendering;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 using UnityEditor.ShaderGraph.Legacy;
+using BlendOp = UnityEditor.ShaderGraph.BlendOp;
 
 namespace UnityEditor.Rendering.Universal.ShaderGraph
 {
     sealed class UniversalUnlitSubTarget : SubTarget<UniversalTarget>, ILegacyTarget
     {
+        const int kOverrideCount = 8;
+
         [Serializable]
         class PassOverride
         {
+            [SerializeField] public bool foldout;
             [SerializeField] public int index;
+            [SerializeField] public bool[] overrides = new bool[kOverrideCount];
 
-            [SerializeField] public bool overrideCull;
-
-            [SerializeField] public Cull cullValue;
-
-
+            [SerializeField] public Cull cullValue; // 0
+            [SerializeField] public Blend blendSrc; // 1
+            [SerializeField] public Blend blendDst; // 1
+            [SerializeField] public BlendOp blendOp; // 2
+            [SerializeField] public BlendOp blendOpAlpha; // 2
+            [SerializeField] public ZTest zTest; // 3
+            [SerializeField] public ZWrite zWrite; // 4
+            [SerializeField] public bool zClip; // 5
+            [SerializeField] public string colorMask; // 6
+            [SerializeField] public bool alphaToMask; // 7
         }
 
         static readonly GUID kSourceCodeGuid = new GUID("97c3f7dcb477ec842aa878573640313a"); // UniversalUnlitSubTarget.cs
 
         [SerializeField]
         List<PassOverride> m_PassOverrides;
-
-        List<bool> m_PassFoldouts = new List<bool>(1);
 
         public UniversalUnlitSubTarget()
         {
@@ -148,18 +156,144 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 true,
                 passOverride => supportedPasses[passOverride.index].descriptor.displayName);
 
+            void PassOverrideElementGUI(Rect rect, int index, GUIContent style, PassOverride passOverride, Action<Rect> drawAction)
+            {
+                var indent = 15;
+                var toggleWidth = 16;
+                var labelWidth = 0.7f;
+                var elementRect = new Rect(rect.x, rect.y + (EditorGUIUtility.singleLineHeight * (index + 1)), rect.width, EditorGUIUtility.singleLineHeight);
+                var toggleRect = new Rect(elementRect.x + indent, elementRect.y, toggleWidth, elementRect.height);
+                var labelRect = new Rect(elementRect.x + indent + toggleWidth, elementRect.y, (EditorGUIUtility.labelWidth * labelWidth) - indent, elementRect.height);
+                var fieldRect = new Rect(elementRect.x + indent + toggleWidth + labelRect.width, elementRect.y, elementRect.width - indent - toggleWidth - labelRect.width, elementRect.height);
+
+                var overrideValue = EditorGUI.Toggle(toggleRect, passOverride.overrides[index]);
+                if(overrideValue != passOverride.overrides[index])
+                {
+                    registerUndo("Change override");
+                    passOverride.overrides[index] = overrideValue;
+                    onChange();
+                }
+
+                using (var disabledScope = new EditorGUI.DisabledGroupScope(!passOverride.overrides[index]))
+                {
+                    EditorGUI.LabelField(labelRect, style);
+                    drawAction(fieldRect);
+                }
+            }
+
             passList.OnDrawElementCallback +=
                 (rect, index, isActive, isFocused) =>
                 {
                     var passOverride = m_PassOverrides[index];
-                    EditorGUI.LabelField(
-                        new Rect(rect.x, rect.y, rect.width / 2, EditorGUIUtility.singleLineHeight),
-                        supportedPasses[passOverride.index].descriptor.displayName, EditorStyles.label);
+                    var foldoutRect = new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight);
+                    passOverride.foldout = EditorGUI.Foldout(foldoutRect, passOverride.foldout, supportedPasses[passOverride.index].descriptor.displayName);
+                    if (passOverride.foldout)
+                    {
+                        PassOverrideElementGUI(rect, 0, new GUIContent("Cull"), passOverride, (r) =>
+                        {
+                            var cullValue = (Cull)EditorGUI.EnumPopup(r, passOverride.cullValue);
+                            if(cullValue != passOverride.cullValue)
+                            {
+                                registerUndo("Change Cull");
+                                passOverride.cullValue = cullValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 1, new GUIContent("Blend"), passOverride, (r) =>
+                        {
+                            var blendSrcValue = (Blend)EditorGUI.EnumPopup(new Rect(r.x, r.y, r.width / 2, r.height), passOverride.blendSrc);
+                            if(blendSrcValue != passOverride.blendSrc)
+                            {
+                                registerUndo("Change Blend");
+                                passOverride.blendSrc = blendSrcValue;
+                                onChange();
+                            }
+                            var blendDstValue = (Blend)EditorGUI.EnumPopup(new Rect(r.x + r.width / 2, r.y, r.width / 2, r.height), passOverride.blendDst);
+                            if(blendDstValue != passOverride.blendDst)
+                            {
+                                registerUndo("Change Blend");
+                                passOverride.blendDst = blendDstValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 2, new GUIContent("BlendOp"), passOverride, (r) =>
+                        {
+                            var blendOpValue = (BlendOp)EditorGUI.EnumPopup(new Rect(r.x, r.y, r.width / 2, r.height), passOverride.blendOp);
+                            if(blendOpValue != passOverride.blendOp)
+                            {
+                                registerUndo("Change BlendOp");
+                                passOverride.blendOp = blendOpValue;
+                                onChange();
+                            }
+                            var blendOpAlphaValue = (BlendOp)EditorGUI.EnumPopup(new Rect(r.x + r.width / 2, r.y, r.width / 2, r.height), passOverride.blendOpAlpha);
+                            if(blendOpAlphaValue != passOverride.blendOpAlpha)
+                            {
+                                registerUndo("Change BlendOp");
+                                passOverride.blendOpAlpha = blendOpAlphaValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 3, new GUIContent("ZTest"), passOverride, (r) =>
+                        {
+                            var zTestValue = (ZTest)EditorGUI.EnumPopup(r, passOverride.zTest);
+                            if(zTestValue != passOverride.zTest)
+                            {
+                                registerUndo("Change ZTest");
+                                passOverride.zTest = zTestValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 4, new GUIContent("ZWrite"), passOverride, (r) =>
+                        {
+                            var zWriteValue = (ZWrite)EditorGUI.EnumPopup(r, passOverride.zWrite);
+                            if(zWriteValue != passOverride.zWrite)
+                            {
+                                registerUndo("Change ZWrite");
+                                passOverride.zWrite = zWriteValue;
+                                onChange();
+                            }
+                        });
+
+                        PassOverrideElementGUI(rect, 5, new GUIContent("ZClip"), passOverride, (r) =>
+                        {
+                            var zClipValue = EditorGUI.Toggle(r, passOverride.zClip);
+                            if(zClipValue != passOverride.zClip)
+                            {
+                                registerUndo("Change ZClip");
+                                passOverride.zClip = zClipValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 6, new GUIContent("ColorMask"), passOverride, (r) =>
+                        {
+                            var colorMaskValue = EditorGUI.DelayedTextField(r, passOverride.colorMask);
+                            if(colorMaskValue != passOverride.colorMask)
+                            {
+                                registerUndo("Change ColorMask");
+                                passOverride.colorMask = colorMaskValue;
+                                onChange();
+                            }
+                        });
+                        PassOverrideElementGUI(rect, 7, new GUIContent("AlphaToMask"), passOverride, (r) =>
+                        {
+                            var alphaToMaskValue = EditorGUI.Toggle(r, passOverride.alphaToMask);
+                            if(alphaToMaskValue != passOverride.alphaToMask)
+                            {
+                                registerUndo("Change AlphaToMask");
+                                passOverride.alphaToMask = alphaToMaskValue;
+                                onChange();
+                            }
+                        });
+                    }
                 };
 
             passList.OnElementHeightCallback += (index) =>
             {
-                return 1 * EditorGUIUtility.singleLineHeight;
+                var foldout = m_PassOverrides[index].foldout;
+                if(foldout)
+                    return (1 + kOverrideCount) * EditorGUIUtility.singleLineHeight;
+                else
+                    return 1 * EditorGUIUtility.singleLineHeight;
             };
 
             passList.GetAddMenuOptions = () =>
@@ -180,17 +314,15 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 (list, itemIndex) =>
                 {
                     registerUndo("Remove pass");
-                    // var pass = supportedPasses.FirstOrDefault(s => s.descriptor.displayName == );
-                    // var passIndex = supportedPasses.IndexOf(.index);
-                    // var passOverride = m_PassOverrides.FirstOrDefault(s => s.index == passIndex);
                     m_PassOverrides.Remove(list[itemIndex]);
                     onChange();
                 };
 
-            // passList.OnListReorderedCallback += (list) =>
-            //     {
-            //         m_PassOverrides = list;
-            //     };
+            passList.OnListReorderedCallback +=
+                (list) =>
+                {
+                    onChange();
+                };
             
             context.AddGUIElement(passList);
         }
@@ -233,7 +365,100 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
             foreach(var passOverride in m_PassOverrides)
             {
                 var pass = supportedPasses[passOverride.index];
-                passes.Add(pass.descriptor, pass.fieldConditions);
+                var descriptor = pass.descriptor;
+                var previousRenderStates = descriptor.renderStates.ToList();
+                var renderStates = new RenderStateCollection();
+
+                // Cull
+                if(passOverride.overrides[0])
+                {
+                    renderStates.Add(RenderState.Cull(passOverride.cullValue));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.Cull);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // Blend
+                if(passOverride.overrides[1])
+                {
+                    renderStates.Add(RenderState.Blend(passOverride.blendSrc, passOverride.blendDst));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.Blend);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // BlendOp
+                if(passOverride.overrides[2])
+                {
+                    renderStates.Add(RenderState.BlendOp(passOverride.blendOp, passOverride.blendOpAlpha));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.BlendOp);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // ZTest
+                if(passOverride.overrides[3])
+                {
+                    renderStates.Add(RenderState.ZTest(passOverride.zTest));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.ZTest);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // ZWrite
+                if(passOverride.overrides[4])
+                {
+                    renderStates.Add(RenderState.ZWrite(passOverride.zWrite));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.ZWrite);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // ZClip
+                if(passOverride.overrides[5])
+                {
+                    renderStates.Add(RenderState.ZClip(passOverride.zClip ? "True" : "False"));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.ZClip);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // ColorMask
+                if(passOverride.overrides[6])
+                {
+                    renderStates.Add(RenderState.ColorMask(passOverride.colorMask));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.ColorMask);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                // AlphaToMask
+                if(passOverride.overrides[7])
+                {
+                    renderStates.Add(RenderState.AlphaToMask(passOverride.alphaToMask ? "True" : "False"));
+                }
+                else
+                {
+                    var item = previousRenderStates.FirstOrDefault(s => s.descriptor.type == RenderStateType.AlphaToMask);
+                    if(item != null) renderStates.Add(item.descriptor);
+                }
+
+                descriptor.renderStates = renderStates;
+                passes.Add(descriptor, pass.fieldConditions);
             }
 
             return new SubShaderDescriptor()
